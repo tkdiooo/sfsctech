fb6dbad5db31edbb9a3f380e88878590e90e8578
tkdiooo <zhangqi_82@163.com>
2017-06-16 17:20:11 +0800

    mybatis add

--- sfsctech-common/common-cache/src/main/java/com/sfsctech/configurer/CacheConfigurer.java
+++ sfsctech-common/common-cache/src/main/java/com/sfsctech/configurer/CacheConfigurer.java
@@ -3,32 +3,19 @@ package com.sfsctech.configurer;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.sfsctech.cache.redis.RedisProperties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.CacheManager;
-import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheManager;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-import redis.clients.jedis.*;
-
-import java.lang.reflect.Method;
-import java.util.HashSet;
-import java.util.Set;
+import redis.clients.jedis.JedisPoolConfig;
 
 /**
  * Class RedisConfigurer
