fc9612d946e5dad35c5e33587888a5d64dd97044
tkdiooo <zhangqi_82@163.com>
2017-10-27 13:03:40 +0800

    dubbo ProtocolConfig config

--- sfsctech-framework/sfsctech-website-jsp/src/main/java/com/sfsctech/website/jsp/service/AccountService.java
+++ sfsctech-framework/sfsctech-website-jsp/src/main/java/com/sfsctech/website/jsp/service/AccountService.java
@@ -1,6 +1,6 @@
 package com.sfsctech.website.jsp.service;
 
-import com.alibaba.dubbo.config.annotation.Reference;
+//import com.alibaba.dubbo.config.annotation.Reference;
 import com.sfsctech.base.model.PagingInfo;
 import com.sfsctech.rpc.result.ActionResult;
 import com.sfsctech.common.util.RandomUtil;
@@ -20,31 +20,33 @@ import java.util.Date;
 @Service
 public class AccountService {
 
-    @Reference
-    private SysAccountService accountService;
+//    @Reference
+//    private SysAccountService accountService;
 
     public void batchSave() {
-        SysAccountDto dto;
-        for (int i = 0; i < 50; i++) {
-            dto = new SysAccountDto();
-            dto.setCreatetime(new Date());
-            dto.setCreator((long) i);
-            dto.setEnabled(0);
-            dto.setLocked(0);
-            dto.setStatus(StatusConstants.Status.Valid.getCode());
-            dto.setAccount(RandomUtil.getRandom(RandomUtil.Strategy.Char, 15));
-            dto.setPassword(RandomUtil.getRandom(RandomUtil.Strategy.Full, 10));
-            System.out.println(accountService.save(dto));
-        }
+//        SysAccountDto dto;
+//        for (int i = 0; i < 50; i++) {
+//            dto = new SysAccountDto();
+//            dto.setCreatetime(new Date());
+//            dto.setCreator((long) i);
+//            dto.setEnabled(0);
+//            dto.setLocked(0);
+//            dto.setStatus(StatusConstants.Status.Valid.getCode());
+//            dto.setAccount(RandomUtil.getRandom(RandomUtil.Strategy.Char, 15));
+//            dto.setPassword(RandomUtil.getRandom(RandomUtil.Strategy.Full, 10));
+//            System.out.println(accountService.save(dto));
+//        }
     }
 
     public ActionResult<SysAccountDto> save(SysAccountDto model) {
         model.setCreatetime(new Date());
         model.setCreator(1L);
-        return accountService.save(model);
+//        return accountService.save(model);
+        return null;
     }
 
     public ActionResult<PagingInfo<SysAccountDto>> findByPage(PagingInfo<SysAccountDto> pagingInfo) {
-        return accountService.findByPage(pagingInfo);
+//        return accountService.findByPage(pagingInfo);
+        return null;
     }
 }
