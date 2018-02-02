67c0e60a9b91286eb6fb550b61fca5cafc4274bb
tkdiooo <zhangqi_82@163.com>
2018-02-02 17:04:47 +0800

    dubbo ProtocolConfig config

---
+++ sfsctech-common/common-util/src/main/java/com/sfsctech/common/util/Cn2SpellUtil.java
@@ -0,0 +1,57 @@
+package com.sfsctech.common.util;
+
+import net.sourceforge.pinyin4j.PinyinHelper;
+import net.sourceforge.pinyin4j.format.HanyuPinyinCaseType;
+import net.sourceforge.pinyin4j.format.HanyuPinyinOutputFormat;
+import net.sourceforge.pinyin4j.format.HanyuPinyinToneType;
+import net.sourceforge.pinyin4j.format.exception.BadHanyuPinyinOutputFormatCombination;
+
+/**
+ * Class Cn2SpellUtil
+ *
+ * @author 张麒 2018-2-2.
+ * @version Description:
+ */
+public class Cn2SpellUtil {
+
+    public static String converterToFirstSpell(String chines) {
+        StringBuilder pinyinName = new StringBuilder();
+        char[] nameChar = chines.toCharArray();
+        HanyuPinyinOutputFormat defaultFormat = new HanyuPinyinOutputFormat();
+        defaultFormat.setCaseType(HanyuPinyinCaseType.LOWERCASE);
+        defaultFormat.setToneType(HanyuPinyinToneType.WITHOUT_TONE);
+        for (char aNameChar : nameChar) {
+            if (aNameChar > 128) {
+                try {
+                    pinyinName.append(PinyinHelper.toHanyuPinyinStringArray(aNameChar, defaultFormat)[0].charAt(0));
+                } catch (BadHanyuPinyinOutputFormatCombination e) {
+                    e.printStackTrace();
+                }
+            } else {
+                pinyinName.append(aNameChar);
+            }
+        }
+        return pinyinName.toString();
+    }
+
+    public static String converterToSpell(String chines) {
+        StringBuilder pinyinName = new StringBuilder();
+        char[] nameChar = chines.toCharArray();
+        HanyuPinyinOutputFormat defaultFormat = new HanyuPinyinOutputFormat();
+        defaultFormat.setCaseType(HanyuPinyinCaseType.LOWERCASE);
+        defaultFormat.setToneType(HanyuPinyinToneType.WITHOUT_TONE);
+        for (char aNameChar : nameChar) {
+            if (aNameChar > 128) {
+                try {
+                    pinyinName.append(PinyinHelper.toHanyuPinyinStringArray(aNameChar, defaultFormat)[0]);
+                } catch (BadHanyuPinyinOutputFormatCombination e) {
+                    e.printStackTrace();
+                }
+            } else {
+                pinyinName.append(aNameChar);
+            }
+            pinyinName.append(",");
+        }
+        return pinyinName.toString();
+    }
+}
