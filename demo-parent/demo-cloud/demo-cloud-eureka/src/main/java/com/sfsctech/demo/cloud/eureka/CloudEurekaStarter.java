e5f132277b3e6267f1daa34ed25e9cd42d42ec3c
tkdiooo <zhangqi_82@163.com>
2018-04-27 17:46:31 +0800

    dubbo ProtocolConfig config

---
+++ demo-parent/demo-cloud/demo-cloud-eureka/src/main/java/com/sfsctech/demo/cloud/eureka/CloudEurekaStarter.java
@@ -0,0 +1,20 @@
+package com.sfsctech.demo.cloud.eureka;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Class CloudEurekaStarter
+ *
+ * @author 张麒 2018-4-27.
+ * @version Description:
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class CloudEurekaStarter {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(CloudEurekaStarter.class).web(true).run(args);
+    }
+}
