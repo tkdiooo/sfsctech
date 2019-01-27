85806fac9d8dc1a06a6aa6e2e925846ad5548099
tkdiooo <zhangqi_82@163.com>
2019-01-27 22:51:53 +0800

    commit

--- support-parent/support-static-resources/src/main/java/com/sfsctech/resources/StaticResourcesStarter.java
+++ support-parent/support-static-resources/src/main/java/com/sfsctech/resources/StaticResourcesStarter.java
@@ -1,10 +1,11 @@
 package com.sfsctech.resources;
 
-import com.sfsctech.constants.LabelConstants;
+import com.sfsctech.core.base.constants.LabelConstants;
 import com.sfsctech.resources.filter.SecurityFilter;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.boot.builder.SpringApplicationBuilder;
-//import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.Ordered;
 
@@ -14,19 +15,19 @@ import org.springframework.core.Ordered;
  * @author 张麒 2017/5/11.
  * @version Description:
  */
-//@SpringBootApplication
+@SpringBootApplication
 public class StaticResourcesStarter {
 
-//    @Bean
-//    public FilterRegistrationBean filterRegistration() {
-//        FilterRegistrationBean registration = new FilterRegistrationBean(new SecurityFilter());
-//        registration.addUrlPatterns(LabelConstants.SLASH_STAR);
-//        registration.setName("SecurityFilter");
-//        registration.setOrder(Ordered.HIGHEST_PRECEDENCE);
-//        return registration;
-//    }
-//
-//    public static void main(String[] args) {
-//        new SpringApplicationBuilder(StaticResourcesStarter.class).web(true).run(args);
-//    }
+    @Bean
+    public FilterRegistrationBean filterRegistration() {
+        FilterRegistrationBean<SecurityFilter> registration = new FilterRegistrationBean<>(new SecurityFilter());
+        registration.addUrlPatterns(LabelConstants.SLASH_STAR);
+        registration.setName("SecurityFilter");
+        registration.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return registration;
+    }
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(StaticResourcesStarter.class).web(WebApplicationType.SERVLET).run(args);
+    }
 }
--- support-parent/support-static-resources/src/main/java/com/sfsctech/resources/controller/IndexController.java
+++ support-parent/support-static-resources/src/main/java/com/sfsctech/resources/controller/IndexController.java
@@ -1,8 +1,7 @@
 package com.sfsctech.resources.controller;
 
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -12,10 +11,10 @@ import javax.servlet.http.HttpServletRequest;
  * @author 张麒 2018-6-4.
  * @version Description:
  */
-//@RestController
+@RestController
 public class IndexController {
 
-//    @PostMapping("index")
+    @PostMapping("index")
     public String index(HttpServletRequest request) {
         System.out.println(request.getParameter("userName"));
         return "index";
